48b6fbfb42eb5b34ce8ed324ff38e6fc2d98a4f7
devnazir <nazirbachrululum@gmail.com>
2020-05-15 21:11:19 +0700

    tahap pengembangan-1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// function menuToggle(e) {
+//     e.target.classList.toggle('close');
+//     e.target.previousElementSibling.classList.toggle('nav-open');
+// }
+
+// const container = document.querySelector('.container');
+// container.addEventListener('click', function (e) {
+//     if (e.target.className == 'menu-toggle') {
+//         menuToggle()
+//         if (e.target.classList.contains('close') || e.target.previousElementSibling.classList.contains('nav-open')) {
+//             e.target.classList.remove('close');
+//             e.target.previousElementSibling.classList.remove('nav-open');
+//         }
+//     }
+// });
