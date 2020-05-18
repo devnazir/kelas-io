7da2c7f3bcfe5ad70d419418d4aef6214ca0bcf9
devnazir <nazirbachrululum@gmail.com>
2020-05-18 20:35:36 +0700

    pengembangan-4

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-// function menuToggle(e) {
-//     e.target.classList.toggle('close');
-//     e.target.previousElementSibling.classList.toggle('nav-open');
-// }
-
-// const container = document.querySelector('.container');
-// container.addEventListener('click', function (e) {
-//     if (e.target.className == 'menu-toggle') {
-//         menuToggle()
-//         if (e.target.classList.contains('close') || e.target.previousElementSibling.classList.contains('nav-open')) {
-//             e.target.classList.remove('close');
-//             e.target.previousElementSibling.classList.remove('nav-open');
-//         }
-//     }
-// });
+const container = document.querySelector('.container');
+container.addEventListener('click', function (e) {
+    if (e.target.className == 'menu-toggle' || e.target.className == 'menu-toggle close') {
+        e.target.parentElement.children[1].classList.toggle('nav-open');
+        if (e.target.parentElement.children[1].classList.contains('nav-open')) {
+            e.target.classList.add('close');
+            document.body.style.overflowY = 'hidden';
+        } else {
+            e.target.classList.remove('close');
+            document.body.style.overflowY = 'visible';
+        }
+    }
+});
