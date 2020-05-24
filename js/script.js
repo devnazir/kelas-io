406e55405b9e6d3c32fa0cdc7d433ffb6f5f3015
devnazir <nazirbachrululum@gmail.com>
2020-05-24 08:09:22 +0700

    update ui

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 const menuToggle = document.querySelector('header nav .menu-toggle');
+const li = document.querySelectorAll('header nav ul li a');
+
 menuToggle.addEventListener('click', function () {
-	menuToggle.classList.toggle('close');
-	menuToggle.parentElement.children[1].classList.toggle('nav-open');
-	if(menuToggle.parentElement.children[1].classList.contains('nav-open')){
-		menuToggle.parentElement.parentElement.parentElement.style.backgroundColor = 'white';
+	this.classList.toggle('close');
+	this.parentElement.children[1].classList.toggle('nav-open');
+	if (this.parentElement.children[1].classList.contains('nav-open')) {
+		this.parentElement.parentElement.parentElement.style.backgroundColor = 'white';
 		document.body.style.overflowY = 'hidden';
 	} else {
 		document.body.style.overflowY = 'visible';
 	}
 });
 
-// window.addEventListener('scroll', function () {
-// 	let wScroll = window.scrollY;
-// 	console.log(wScroll)
-// 	const navbar = document.querySelector('header nav');
-// 	if (wScroll > 450) {
-// 		navbar.classList.add('setNav');
-// 	} else if (wScroll <= 450) {
-// 		navbar.classList.remove('setNav');
-// 	}
-// });
+li.forEach((link) => {
+	link.addEventListener('click', function () {
+		document.body.style.overflowY = 'visible';
+		this.parentElement.parentElement.classList.remove('nav-open');
+		this.parentElement.parentElement.nextElementSibling.classList.remove('close');
+	});
 
-window.addEventListener('load', function(){
+});
+
+window.addEventListener('load', function () {
 	let overlay = document.querySelector('.overlay');
 	overlay.style.display = 'none';
 });
